6a35df8e251fcb20f5c9638f90ef95773d6396d7
Gen2micky1 <olowofelamicheal1@gmail.com>
2024-11-09 11:43:27 -0800

    my python wek1 assignment solution

---
+++ user_input.py
@@ -0,0 +1,7 @@
+# Personalized Message
+
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+location = input("Where are you located? ")
+
+print("Hello," ,name, "you are", age, "years old and live in", location,".")
